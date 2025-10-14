eea9175adef9a129b2ffff6aed0ddd5a3c164bb2
magwindow <antosha.petrakov@yandex.ru>
2025-10-14 14:35:13 +0300

    Create cart app. Added selection and display of the number of products, adding and removing from the basket, calculating the total amount

--- app/settings.py
+++ app/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -114,3 +115,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CART_SESSION_ID = 'cart'
