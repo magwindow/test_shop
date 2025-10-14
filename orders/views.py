cba05963b67eeb76c7bc28c8d7c96a5a8b14f247
magwindow <antosha.petrakov@yandex.ru>
2025-10-14 18:44:46 +0300

    Added celery, flower, rabbitmq

--- orders/views.py
+++ orders/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from .models import OrderItem
 from .forms import OrderCreateForm
+from .tasks import order_created
 from cart.cart import Cart
 
 
@@ -18,6 +19,8 @@ def order_create(request):
                                          quantity=item['quantity'])
             # очистить корзину
             cart.clear()
+            # запустить асинхронное задание
+            order_created.delay(order.id)
             return render(request, 'orders/order/created.html', {'order': order})
     else:
         form = OrderCreateForm()
